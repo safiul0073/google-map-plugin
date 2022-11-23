37ff2c3f00bb7eef344e6f4a1459b96225232e3e
safiul-dev <parsonal494@gmail.com>
2022-11-23 17:04:22 +0600

    google map short code added

--- src/includes/SPP_Main.php
+++ src/includes/SPP_Main.php
@@ -6,26 +6,38 @@ use Src\admin\SPP_Manu;
 use Src\admin\SPP_SectionField;
 use Src\API\AdminCallback;
 use Src\includes\SPP_Loader;
+use Src\public\SPP_GoogleMap;
+
 class SPP_Main {
 
     protected $loader;
     protected $admin_menu;
     protected $admin_setting;
+    protected $public_setting;
     function __construct()
     {
         $this->loadDependencis();
         $this->loadAdminHook();
+        $this->loadPublicHook();
     }
 
     function loadDependencis () {
         $this->loader         = new SPP_Loader();
         $this->admin_menu     = new SPP_Manu();
         $this->admin_setting  = new SPP_SectionField();
+        $this->public_setting = new SPP_GoogleMap();
     }
 
     function loadAdminHook () {
         $this->loader->addAction( 'admin_menu', $this->admin_menu, 'addMenuPage' );
         $this->loader->addAction( 'admin_init', $this->admin_setting, 'registerCustomFields');
+    }
+
+    function loadPublicHook () {
+        add_shortcode( 'show_google_map', array($this->public_setting, 'shortCode') );
+		$this->loader->addAction( 'wp_enqueue_style', $this->public_setting, 'enqueueStyle' );
+		$this->loader->addAction( 'wp_enqueue_scripts', $this->public_setting, 'enqueueScript' );
+
     }
 
     function getLoader () {
--- src/public/SPP_GoogleMap.php
+++ src/public/SPP_GoogleMap.php
@@ -14,10 +14,10 @@ class SPP_GoogleMap extends BaseController{
         $api_key = esc_attr(get_option( 'google_map_api_key' ));
 
         wp_register_script('googlemaps', 'https://maps.googleapis.com/maps/api/js?key='.$api_key.'&v=weekly' , array(), '', true);
-        wp_enqueue_script( $this->pluginName(), $this->getPluginUrl() . 'js/google.map.js', array( 'jquery', 'googlemaps' ), $this->version(), true );
+        wp_enqueue_script( $this->pluginName(), $this->getPluginUrl() . 'js/google-map.js', array( 'jquery', 'googlemaps' ), $this->version(), true );
     }
 
     function shortCode( ) {
-        return require_once plugin_dir_path( dirname( __FILE__ ) ) . 'public/spp-google-map.php';
+        return require_once plugin_dir_path( dirname( __FILE__ )  ) . 'public/spp-google-map-html.php';
     }
 }
